3fac326d63a402b1ed1876cad56226977829040f
Levi Perelman <levi@impulsedsp.com>
2023-08-02 16:25:15 +0300

    Added the webcam emulation video into the repo.

--- core/fixtures.py
+++ core/fixtures.py
@@ -1,15 +1,23 @@
+from os import path
+
 import pytest
 from selenium.webdriver.chrome import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 
+VIDEO_FILE = "./assets/video.y4m"
+
 
 @pytest.fixture(scope="session")
 def driver() -> WebDriver:
-    video_file = "/home/levi/Downloads/video.y4m"
+    video_file_path = path.abspath(VIDEO_FILE)
+    if not path.exists(video_file_path):
+        raise FileNotFoundError(
+            f"Video file for mocking webcam not found: {video_file_path}"
+        )
 
     options = webdriver.Options()
     options.add_argument("--use-fake-device-for-media-stream")
-    options.add_argument(f"--use-file-for-fake-video-capture={video_file}")
+    options.add_argument(f"--use-file-for-fake-video-capture={video_file_path}")
 
     driver = WebDriver(options=options)
 
--- tests/main_test.py
+++ tests/main_test.py
@@ -3,4 +3,4 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 
 def test_webcam_site(driver: WebDriver) -> None:
     driver.get("http://localhost:3000")
-    assert driver.title == "Webcam"
+    assert driver.title == "Webcam Playground"
